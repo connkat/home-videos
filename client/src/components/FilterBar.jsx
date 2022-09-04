c6ddeb1ce7d08d0220266e5264254e0009fedc2b
connkat <hi.connkat@gmail.com>
2022-09-04 11:07:20 -0700

    filterbar setup

---
+++ client/src/components/FilterBar.jsx
@@ -0,0 +1,15 @@
+
+function FilterBar() {
+  return (
+    <div className="filter-bar">
+        <p>search term</p>
+        <p>year</p>
+        <p>month</p>
+        <p>holiday</p>
+        <p>main</p>
+        <p>others</p>
+    </div>
+  );
+}
+
+export default FilterBar;
