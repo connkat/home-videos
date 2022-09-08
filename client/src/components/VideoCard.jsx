c8a3d414bdd82074d3643aea932eea0ed9ea1878
connkat <hi.connkat@gmail.com>
2022-09-07 23:39:40 -0700

    added video player comp

--- client/src/components/VideoCard.jsx
+++ client/src/components/VideoCard.jsx
@@ -1,12 +1,13 @@
-
+import VideoPlayer from './VideoPlayer.jsx';
 function VideoCard() {
   return (
     <div className="video-card">
         <p>year</p>
         <p>month</p>
         <p>holiday</p>
-        <p>family Member</p>
+        <p>family member name</p>
         <p>others</p>
+        <VideoPlayer />
     </div>
   );
 }
