2b0d4882b40842fdf10b008759ca4b95080b0f14
connkat <hi.connkat@gmail.com>
2022-09-08 07:25:04 -0700

    updated embded video

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,5 +1,17 @@
-function VideoPlayer() {
-  return <div className="video-player"></div>;
-}
+import PropTypes from "prop-types";
+
+const VideoPlayer = ({ embedId }) => (
+  <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
 
 export default VideoPlayer;
