eef809904a9875bceae89029148dd9ded354f33d
connkat <hi.connkat@gmail.com>
2022-09-14 22:17:19 -0600

    form submit

--- client/src/components/FilterBar/FilterBar.jsx
+++ client/src/components/FilterBar/FilterBar.jsx
@@ -1,16 +1,38 @@
-import TextField from "./TextField"
+import { useState } from "react";
+import TextField from "./TextField";
 
-import './filterbar.css';
+import "./filterbar.css";
 
 function FilterBar() {
+  const [searchField, setSearchField] = useState("");
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { field };
+      const res = await fetch("http://localhost:4000/videos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      setSearchField("");
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
   return (
     <div className="filterbar">
-        <TextField label="search"/>
+      <form>
+        <TextField label="search" />
         <p>year</p>
         <p>month</p>
         <p>holiday</p>
         <p>family Member</p>
         <p>others</p>
+        <button type="submit" onClick={onSubmitForm}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
