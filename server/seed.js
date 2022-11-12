2ec8d8efe67d4f78a44ad621b610b4cb98d04f2e
connkat <hi.connkat@gmail.com>
2022-11-11 21:58:03 -0700

    format and second video

--- server/seed.js
+++ server/seed.js
@@ -1,1 +1,4 @@
-date = "March 12", year = "1997", embedId = "hzSSQ7_9enI"
+const videos = [
+  { date: "March 12", year: "1997", embedId: "hzSSQ7_9enI" },
+  { date: "April 30", year: "1994", embedId: "MdkGe8wyd5c" },
+];
