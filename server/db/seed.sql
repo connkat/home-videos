101524620880254183e56d8e31363fc2a958d169
connkat <hi.connkat@gmail.com>
2022-11-22 23:31:58 -0700

    added second seed

--- server/db/seed.sql
+++ server/db/seed.sql
@@ -3,3 +3,6 @@ VALUES ('Kate', 'test', 'test');
 
 INSERT INTO videos (date, year, embedId, main, others, event)
 VALUES ('March 12', 1997, 'hzSSQ7_9enI', null, null null)
+
+INSERT INTO videos (date, year, embedId, main, others, event)
+VALUES ('April 30', 1994, 'MdkGe8wyd5c', null, null null)
